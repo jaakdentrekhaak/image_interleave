b44c851dca6e39d1ebb7384fa3cd2e59815e6f47
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-01-01 10:45:46 +0100

    add different size image support

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import tkinter, tkinter.filedialog
+import os
 
 # Open tkinter filedialog window to select paths
 root = tkinter.Tk()
@@ -11,6 +12,8 @@ path1 = tkinter.filedialog.askopenfilename()
 print('Select second image')
 path2 = tkinter.filedialog.askopenfilename()
 
+print('Processing...')
+
 root.destroy()
 
 # load images
@@ -20,8 +23,8 @@ img2 = cv2.imread(path2)
 def interleave(first, second):
     """
     Takes pixels in checkerboard pattern from second image to replace the pixels from the first image.
-    :param first: numpy array that represents a matrix of pixels from an image
-    :param second: numpy array that represents a matrix of pixels from an image the same size as the first image (doesn't fail if not the case)
+    :param first: numpy array that represents a matrix of pixels from the first image
+    :param second: numpy array that represents a matrix of pixels from the second image the same size as the first image
     :return: numpy array that represents a matrix of pixels from an image, but pixels have been interleaved in checkerboard pattern
     """
     # get image size
@@ -39,10 +42,53 @@ def interleave(first, second):
     
     return res
 
+def preprocess(first, second):
+    """
+    Sets the size of both images equal by taking the smallest width and height and thus cutting off other pixels.
+    :param first: numpy array that represents a matrix of pixels from the first image
+    :param second: numpy array that represents a matrix of pixels from the second image
+    :return: two numpy arrays with containing modified images
+    """
+
+    # get image size
+    height1 = len(first)
+    width1 = len(first[0])
+    height2 = len(second)
+    width2 = len(second[0])
+
+    # Minimum size
+    height_min = min(height1, height2)
+    width_min = min(width1, width2)
+
+    # initialize new arrays
+    first_mod = np.empty([height_min, width_min, 3]) # 3 because RGB
+    second_mod = np.empty([height_min, width_min, 3])
+
+    # Set pixels of both images
+    for row in range(height_min):
+        for col in range(width_min):
+            first_mod[row][col] = first[row][col]
+            second_mod[row][col] = second[row][col]
+    
+    return first_mod, second_mod
+
+
+
 ##########
 
-modified = interleave(img1, img2)
+mod1, mod2 = preprocess(img1, img2)
 
-cv2.imshow('image', modified)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+modified = interleave(mod1, mod2)
+
+# # # Show image
+# # cv2.imshow('image', modified)
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
+
+# Set working directory to save location
+image_dir = '/'.join(path1.split('/')[:-1]) + '/'
+os.chdir(image_dir)
+
+cv2.imwrite('interleaved.jpg', modified)
+
+print('Successfully saved at: ', image_dir)
